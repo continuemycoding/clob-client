44ff08eaa6280c99b59a4135478fc44730e6917a
test <test@gmail.com>
2024-12-27 21:13:09 +0800

    x

--- src/app.ts
+++ src/app.ts
@@ -19,22 +19,22 @@ const Yes = 0;
 const No = 1;
 
 const trades = {
-    "0x87d67272f0ce1bb0d80ba12a1ab79287b2a235a5f361f5bcbc06ea0ce34e61c5": Yes, // Will there be a US Government shutdown?
-    "0x1ba85a54b6ff5db0d5f345bb07c2466850e476a8a735a6b82d407222a19b8a07": Yes, // Will Elon tweet 250-274 times Dec 20-27?
-    "0x7d65c2360ae87c27b252cfb41356914e80187659be5685fb65da8e17ccfd215d": Yes, // Will Elon tweet 275-299 times Dec 20-27?
-    "0x055f0838ccbaafce2a0d694d20ffb815cb0b5bb85667fee55cce958a7fe89c5a": Yes, // Will Elon tweet 300-324 times Dec 20-27?
-    "0xdc7d3eba0d5c91f58cc90626065c95243fc2d9b47ce9dfe1ab4341e230b6dc84": Yes, // Will Elon tweet 325-349 times Dec 20-27?
-    "0x643a489de21c4c07d50065a90cb44f3b3e746a54660b940eaf21a1d9e4dc4a87": Yes, // Will Elon tweet 350-374 times Dec 20-27?
-    "0x67500eddcbf5fe7d5e5ec16b67c212eb58e462845a0bb10bf4401c48088bbd07": Yes, // Will Elon tweet 375-399 times Dec 20-27?
-    "0xf56b00519c0841f123302402a247d0241acd93a22e1a1cc8a7a557abe6e34dc7": Yes, // Will Elon tweet 400-424 times Dec 20-27?
-    "0xe18a5a9d08e3f89798244959c20d198d13ab5d8230ee48c1b8201f73ae969ffb": Yes, // Will Elon tweet 425-449 times Dec 20-27?
-    "0x8dea7119588d217a183b0d31bb5d3acc220986a1bb95976b2d02858d8b37eb35": Yes, // Will Elon tweet 450-474 times Dec 20-27?
-    "0x3e388cdb2df676ec02935cf75a535d764cb8dc7cd997dab18b3779df02a263de": Yes, // Will Elon tweet 475-499 times Dec 20-27?
+    // "0x87d67272f0ce1bb0d80ba12a1ab79287b2a235a5f361f5bcbc06ea0ce34e61c5": Yes, // Will there be a US Government shutdown?
+    // "0x1ba85a54b6ff5db0d5f345bb07c2466850e476a8a735a6b82d407222a19b8a07": Yes, // Will Elon tweet 250-274 times Dec 20-27?
+    // "0x7d65c2360ae87c27b252cfb41356914e80187659be5685fb65da8e17ccfd215d": Yes, // Will Elon tweet 275-299 times Dec 20-27?
+    // "0x055f0838ccbaafce2a0d694d20ffb815cb0b5bb85667fee55cce958a7fe89c5a": Yes, // Will Elon tweet 300-324 times Dec 20-27?
+    // "0xdc7d3eba0d5c91f58cc90626065c95243fc2d9b47ce9dfe1ab4341e230b6dc84": Yes, // Will Elon tweet 325-349 times Dec 20-27?
+    // "0x643a489de21c4c07d50065a90cb44f3b3e746a54660b940eaf21a1d9e4dc4a87": Yes, // Will Elon tweet 350-374 times Dec 20-27?
+    // "0x67500eddcbf5fe7d5e5ec16b67c212eb58e462845a0bb10bf4401c48088bbd07": Yes, // Will Elon tweet 375-399 times Dec 20-27?
+    // "0xf56b00519c0841f123302402a247d0241acd93a22e1a1cc8a7a557abe6e34dc7": Yes, // Will Elon tweet 400-424 times Dec 20-27?
+    // "0xe18a5a9d08e3f89798244959c20d198d13ab5d8230ee48c1b8201f73ae969ffb": Yes, // Will Elon tweet 425-449 times Dec 20-27?
+    // "0x8dea7119588d217a183b0d31bb5d3acc220986a1bb95976b2d02858d8b37eb35": Yes, // Will Elon tweet 450-474 times Dec 20-27?
+    // "0x3e388cdb2df676ec02935cf75a535d764cb8dc7cd997dab18b3779df02a263de": Yes, // Will Elon tweet 475-499 times Dec 20-27?
 
-    "0xf8df5cd1f0f97916b35c96743242a2f4ca377bf5c3e3f608f0d02196d36deae5": Yes, // Will MicroStrategy purchase more Bitcoin in 2024?
-    "0x7b0f6f3b168bfeeb8356a2e525d0566bd54118d79a44433485a1ddef9b32dee2": Yes, // Will OpenAI have the top AI model on January 31?
-    "0xc4f606569acc4d2871bf0cae1b53d0a12dae9f289d2f1011b4ead72b066ac00a": Yes, // Will Google have the top AI model on January 31?
-    "0xa5ac4cdcfff44ddfb0d332d33575f766414465786fb7d4350782db40d5e9da11": Yes, // Trump ends Ukraine war by first 90 days?
+    // "0xf8df5cd1f0f97916b35c96743242a2f4ca377bf5c3e3f608f0d02196d36deae5": Yes, // Will MicroStrategy purchase more Bitcoin in 2024?
+    // "0x7b0f6f3b168bfeeb8356a2e525d0566bd54118d79a44433485a1ddef9b32dee2": Yes, // Will OpenAI have the top AI model on January 31?
+    // "0xc4f606569acc4d2871bf0cae1b53d0a12dae9f289d2f1011b4ead72b066ac00a": Yes, // Will Google have the top AI model on January 31?
+    // "0xa5ac4cdcfff44ddfb0d332d33575f766414465786fb7d4350782db40d5e9da11": Yes, // Trump ends Ukraine war by first 90 days?
     "0x97587c58a3407fcc9a8df6396aaa8b66eff8b0c799fdf81880f258755b7d529c": Yes, // Will Bitcoin hit $100k again in 2024?
 };
 
@@ -152,12 +152,12 @@ interface MarketData {
     }, {});
 
     const openOrders = await clobClient.getOpenOrders();
-    for (const { asset_id, price, side, original_size } of openOrders) {
+    for (const { asset_id, price, side, size_matched, original_size } of openOrders) {
         userOrders[asset_id] = {
             tokenID: asset_id,
             price: Number(price),
             side: Side[side as keyof typeof Side],
-            size: Number(original_size),
+            size: Number(original_size) - Number(size_matched),
             status: OrderStatus.Submitted
         };
     }
@@ -183,73 +183,6 @@ interface MarketData {
     app.listen(80, () => {
         console.log(`服务器正在运行`);
     });
-
-    // app.post('/api/login', async (req: Request, res: Response) => {
-    //     const { account, password, otpCode } = req.body;
-
-    //     const key = "0D2A6D83-4EC5-4589-80FB-1FA9C3EB89A4";
-    //     const hashedPassword = crypto.createHmac('md5', key).update(password, "utf-8").digest('hex');
-
-    //     const { results: [user] } = await MysqlHelper.query(`select * from bg_account where account = ? and password = ?`, [account, hashedPassword]);
-    //     if (!user) {
-    //         res.status(403).json({ message: '账号或密码错误' });
-    //         return;
-    //     }
-
-    //     if (!user.otpSecret) {
-    //         res.status(403).json({ message: '安全码秘钥未配置' });
-    //         return;
-    //     }
-
-    //     // 设置 window = 1 后，支持前 30 秒、当前 30 秒、以及后 30 秒的 OTP，总有效时间是 90 秒。
-    //     authenticator.options = { window: 1 };
-
-    //     if (!otpCode || !authenticator.check(otpCode, user.otpSecret)) {
-    //         res.status(403).json({ message: '安全码错误' });
-    //         return;
-    //     }
-
-    //     const token = jwt.sign({ user }, process.env.SECRET_KEY, { expiresIn: '1h' });
-
-    //     const { results: games } = await MysqlHelper.query("select moduleID, moduleName, moduleEnName, kindID from ps_game_module where enable = 1 order by kindID, sortID");
-
-    //     res.json({ token, games });
-    // });
-
-    // app.use((req: Request, res: Response, next: NextFunction) => {
-    //     const token = req.query.token as string || req.headers['authorization']?.split(' ')[1];
-
-    //     if (!token) {
-    //         console.log(req.ip, req.path, 'No token provided');
-    //         res.status(401).json({ message: 'No token provided' });
-    //         return;
-    //     }
-
-    //     jwt.verify(token, process.env.SECRET_KEY, (error: VerifyErrors, decoded: string | JwtPayload) => {
-    //         if (error) {
-    //             res.status(401).json({ message: 'token已过期，请重新登录' });
-    //             return;
-    //         }
-
-    //         const payload = decoded as JwtPayload;
-
-    //         // 如果剩余有效时间小于总有效时间的一半，则刷新 token
-    //         if (payload.exp - Date.now() / 1000 < (payload.exp - payload.iat) / 3) {
-    //             console.log("token临近过期");
-    //             const token = jwt.sign({ user: payload.user }, process.env.SECRET_KEY, { expiresIn: '1h' });
-    //             res.setHeader('token', token);
-    //         }
-
-    //         for (const key in req.body) {
-    //             if (typeof req.body[key] == 'string' && !isNaN(Number(req.body[key]))) {
-    //                 req.body[key] = Number(req.body[key]);
-    //             }
-    //         }
-
-    //         // req.adminUserInfo = payload.user;
-    //         next();
-    //     });
-    // });
 
     app.post(GET_EARNINGS_FOR_USER_FOR_DAY, async (req: Request, res: Response) => {
         const rewardPercentages = await clobClient.getRewardPercentages();
@@ -285,40 +218,6 @@ interface MarketData {
         res.json(response);
     });
 
-    app.post(POST_ORDER, async (req: Request, res: Response) => {
-        const { balance: balanceAmount } = await clobClient.getBalanceAllowance({ asset_type: AssetType.COLLATERAL });
-        const balance = Number(balanceAmount) / 10 ** 6;
-        console.log("余额", balance);
-        // console.log(await clobClient.getBalanceAllowance({ asset_type: AssetType.CONDITIONAL, token_id: "61870696561549212427703774084341694590597083144015451858728593820052569648622" }));
-
-        const price = 0.01;
-
-        const order = await clobClient.createOrder({
-            tokenID: "",//tokens[0].token_id,
-            price,//min: 0.01 - max: 0.99
-            side: Side.BUY,
-            size: 5//Math.floor(balance / price)
-        });
-
-
-
-        // const order = await clobClient.createOrder({
-        //     tokenID: tokens[1].token_id,
-        //     price: 0.86,//min: 0.01 - max: 0.99
-        //     side: Side.BUY,
-        //     size: 5
-        // });
-
-        // const order = await clobClient.createOrder({
-        //     tokenID: tokens[1].token_id,
-        //     side: Side.SELL,
-        //     price: 0.15,
-        //     size: 6
-        // });
-
-        console.log(await clobClient.postOrder(order));
-    });
-
     app.post(CANCEL_ORDER, async (req: Request, res: Response) => {
         const response = await clobClient.cancelOrder(req.body);//orderID
         res.json(response);
@@ -343,8 +242,15 @@ interface MarketData {
         const existingOrder = userOrders[token_id];
 
         if (existingOrder) {
-            if (existingOrder.status != OrderStatus.Submitted)
+            if (existingOrder.status === OrderStatus.PendingCancellation) {
+                console.log(market.question, '等待订单取消完成');
                 return;
+            }
+
+            if (existingOrder.status === OrderStatus.PendingSubmission) {
+                console.log(market.question, '等待订单提交完成');
+                return;
+            }
 
             if (Math.abs(existingOrder.price - midpoint) > rewards_max_spread) {
                 console.log(market.question, "没有奖励就取消订单");
@@ -401,7 +307,7 @@ interface MarketData {
                     size: Math.floor(balance / price),
                 };
 
-                if (balance > price * userOrderParams.size) {
+                if (balance >= price * userOrderParams.size) {
                     userOrders[token_id] = { ...userOrderParams, status: OrderStatus.PendingSubmission };
                     const userOrder = userOrders[token_id];
 
