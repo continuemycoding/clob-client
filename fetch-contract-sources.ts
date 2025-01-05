d354cc4db13deeeb65a32ccc17e8f1636baf269a
test <test@gmail.com>
2025-01-05 21:52:51 +0800

    1

--- fetch-contract-sources.ts
+++ fetch-contract-sources.ts
@@ -6,9 +6,14 @@ import fs from 'fs';
     const url = `https://api.polygonscan.com/api?module=contract&action=getsourcecode&address=${address}&apikey=${process.env.POLYGONSCAN_API_KEY}`;
     const response = await axios.get(url);
     const code = response.data.result[0].SourceCode;
-    const { sources } = JSON.parse(code.substring(1, code.length - 1));
-    for (const key in sources) {
-        const name = key.substring(key.lastIndexOf('/') + 1);
-        fs.writeFileSync(`Contract/${name}`, sources[key].content);
+    try {
+        const { sources } = JSON.parse(code.substring(1, code.length - 1));
+        for (const key in sources) {
+            const name = key.substring(key.lastIndexOf('/') + 1);
+            fs.writeFileSync(`Contract/${name}`, sources[key].content);
+        }
+    }
+    catch (e) {
+        fs.writeFileSync(`Contract.sol`, code);
     }
 })();
